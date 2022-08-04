4b01ae9baa2151baa82f4903e4182b2b825f64b4
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-08-04 16:37:05 +0530

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: ${(props) => props.type !== "sm" && "360px"};
+  margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45px")};
+  cursor: pointer;
+  display: ${(props) => props.type === "sm" && "flex"};
+  gap: 10px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
+  background-color: #999;
+  flex: 1;
+`;
+
+const Details = styled.div`
+  display: flex;
+  margin-top: ${(props) => props.type !== "sm" && "16px"};
+  gap: 12px;
+  flex: 1;
+`;
+
+const ChannelImage = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+  display: ${(props) => props.type === "sm" && "none"};
+`;
+
+const Texts = styled.div``;
+
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.h2`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin: 9px 0px;
+`;
+
+const Info = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Card = ({ type }) => {
+  return (
+    <Link to="/video/test" style={{ textDecoration: "none" }}>
+      <Container type={type}>
+        <Image
+          type={type}
+          src="https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305__340.jpg"
+        />
+        <Details type={type}>
+          <ChannelImage
+            type={type}
+            src="https://cdn2.photostockeditor.com/t/0601/person-person-holding-white-rose-flower-flower-image.jpg"
+          />
+          <Texts>
+            <Title>Test Video</Title>
+            <ChannelName>Unishare</ChannelName>
+            <Info>660,908 views • 1 day ago</Info>
+          </Texts>
+        </Details>
+      </Container>
+    </Link>
+  );
+};
+
+export default Card;
